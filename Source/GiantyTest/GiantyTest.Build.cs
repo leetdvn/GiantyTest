3f6c198658082c4cc3cb4d667e0d0f199fea5b11
leetdvn <111244237+leetdvn@users.noreply.github.com>
2024-05-04 07:29:07 +0700

    Initial commit

---
+++ Source/GiantyTest/GiantyTest.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GiantyTest : ModuleRules
+{
+	public GiantyTest(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
